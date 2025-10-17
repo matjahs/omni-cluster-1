c78798f175606740229e0bea306451dc9ae26321
Matjah Sonnveld <Matjah.Sonneveld@radboudumc.nl>
2025-10-17 09:50:44 +0200

    initial commit

--- examples/terraform/advanced/modules/bootstrap_token/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bootstrap_token" {
-  value = "${random_string.token_prefix.result}.${random_string.token_suffix.result}"
-}
--- examples/terraform/advanced/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  description = "A name to provide for the Talos cluster"
-  type        = string
-}
-
-variable "iso_path" {
-  description = "Path to the Talos ISO"
-  type        = string
-}
